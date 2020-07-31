0f2918ad77ea2eac2d8830f9f4cc8e7583ef8622
Rohit Ghodeswar <rohitghodeswar@gmail.com>
2020-07-31 23:34:31 +0530

    added build folder

---
+++ build/precache-manifest.67e645a7abb6b5bbdd407bdff54bc8ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eaa9e23db20519aa5dfdb129ff91003",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a57f978d5d3431d9e8f0",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c81b1d8391531b34b36c",
+    "url": "/static/js/2.eccc5121.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.eccc5121.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a57f978d5d3431d9e8f0",
+    "url": "/static/js/main.7f0b8e8a.chunk.js"
+  },
+  {
+    "revision": "b1ee046e0ebcfc81d1f0",
+    "url": "/static/js/runtime-main.c3ddb213.js"
+  }
+]);
